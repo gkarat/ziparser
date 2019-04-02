5f0e47522c2c24ee9cd13f04b2609027c03fc2cd
Georgiy Karataev <georgkarat@gmail.com>
2019-04-02 19:42:37 +0200

    html parser implemented

--- ukol8.py
+++ ukol8.py
@@ -3,42 +3,64 @@ import sys
 import os
 import shutil
 import subprocess
+from bs4 import BeautifulSoup
+
 
 ### We are using types[] for keeping all possible types in ZIP
 types = []
 
+
 ### Remove folder with the same ZIP name, if already exists and create once again
-def createTargetPath(path):
-	if os.path.exists(path):
+def createTargetPath(archivePath):
+	if os.path.exists(archivePath):
 		print("Folder with the same name detected...\nRemoving...")
-		shutil.rmtree(path)
+		shutil.rmtree(archivePath)
 
-	os.mkdir(path)
-	print ("Successfully created the directory %s " % path)
+	os.mkdir(archivePath)
+	print ("Successfully created the directory %s " % archivePath)
 
 
-### Copies a given file f from archive ZIP to new folder path (without creating subfolders!)
-def copyFile(archive, f, path, fName):
+### Copies a given file f from archive ZIP to new folder archivePath (without creating subfolders!)
+def copyFile(archive, f, archivePath, fName):
 	source = archive.open(f)
-	target = open(os.path.join(path, fName), "wb")
+	target = open(os.path.join(archivePath, fName), "wb")
 
 	with source, target:
 		shutil.copyfileobj(source, target)
 
 	target.close()
+	source.close()
+
 
 ### Checks if type of the file's content already has its folder. If not, creates it
 ### Finally, moves the file to its relying folder
-def classifyFile(typeName, path, fName):
+def classifyFile(typeName, archivePath, fName):
 	if not (typeName in types):
-		os.mkdir(path + "/" + typeName)
+		os.mkdir(archivePath + "/" + typeName)
 		types.append(typeName)
 
-	shutil.move(path + "/" + fName, path + "/" + typeName + "/" + fName)
+	shutil.move(archivePath + "/" + fName, archivePath + "/" + typeName + "/" + fName)
+
+
+### In case of HTML we use that function, which removes all HTML, CSS tags and JavaScript scripts
+### Then saves it into the overall "texts.html"
+def htmlFileParse(archive, f, archivePath):
+
+	htmlContent = archive.read(f)
+	
+	soup = BeautifulSoup(htmlContent.decode("utf-8"), "lxml")
+	
+	for script in soup(["script", "style"]): # remove all javascript and stylesheet code
+		script.extract()
+	
+	textContent = soup.get_text()
+
+	with open(archivePath + '/texts.html', 'a+') as target:
+		target.write(textContent)
 
 
 ### Extracts file from ZIP and save it in the folder it relies to (classifies it)
-def copyAndClassify(f, path, archive):
+def copyAndClassify(f, archivePath, archive):
 	fName = os.path.basename(f)
 	fType = fName.split('.')[-1]
 
@@ -46,27 +68,30 @@ def copyAndClassify(f, path, archive):
 	if not fName or fType.lower() in ['jpeg', 'jpg', 'png', 'gif']:
 		return
 
-	copyFile(archive, f, path, fName)
+	if fType.lower() in ['html', 'htm']:
+		htmlFileParse(archive, f, archivePath)
+		return
+
+	copyFile(archive, f, archivePath, fName)
 
 	### We request shell for finding the type of the content
-	typeRequest = subprocess.check_output('file "%s/%s"' % (path, fName), shell=True).decode("utf-8") 
+	typeRequest = subprocess.check_output('file "%s/%s"' % (archivePath, fName), shell=True).decode("utf-8") 
 	typeName = typeRequest.split(": ")[1].replace('/', ' ')
 
-	classifyFile(typeName, path, fName)
+	classifyFile(typeName, archivePath, fName)
 
 
 if __name__ == "__main__":
 
-	path = "websoubory.zip"[:-4]
+	archivePath = "websoubory.zip"[:-4]
 
 	### Path to our ZIP without .zip ending
-	# path = sys.argv[1][:-4]
+	# archivePath = sys.argv[1][:-4]
 
-	archive = zipfile.ZipFile(path+".zip", 'r')
+	archive = zipfile.ZipFile(archivePath+".zip", 'r')
 
-	createTargetPath(path)
-
+	createTargetPath(archivePath)
 
 	for f in archive.namelist():
 
-		copyAndClassify(f, path, archive)
+		copyAndClassify(f, archivePath, archive)
