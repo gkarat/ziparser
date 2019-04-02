f1a6c380053fa94aa1f7133cf6dc048c60b6b465
Georgiy Karataev <georgkarat@gmail.com>
2019-04-02 20:08:07 +0200

    editing and commenting

--- ukol8.py
+++ ukol8.py
@@ -20,10 +20,13 @@ def createTargetPath(archivePath):
 	print ("Successfully created the directory %s " % archivePath)
 
 
-### Copies a given file f from archive ZIP to new folder archivePath (without creating subfolders!)
-def copyFile(archive, f, archivePath, fName):
+### Copies a given file f from archive ZIP to new folder with the same name as ZIP (without creating subfolders!)
+def copyFile(archive, f, fName):
+
+	targetPath = archive.filename[:-4]
+
 	source = archive.open(f)
-	target = open(os.path.join(archivePath, fName), "wb")
+	target = open(os.path.join(targetPath, fName), "wb")
 
 	with source, target:
 		shutil.copyfileobj(source, target)
@@ -34,17 +37,20 @@ def copyFile(archive, f, archivePath, fName):
 
 ### Checks if type of the file's content already has its folder. If not, creates it
 ### Finally, moves the file to its relying folder
-def classifyFile(typeName, archivePath, fName):
+def classifyFile(typeName, targetPath, fName):
+	
 	if not (typeName in types):
-		os.mkdir(archivePath + "/" + typeName)
+		os.mkdir(targetPath + "/" + typeName)
 		types.append(typeName)
 
-	shutil.move(archivePath + "/" + fName, archivePath + "/" + typeName + "/" + fName)
+	shutil.move(targetPath + "/" + fName, targetPath + "/" + typeName + "/" + fName)
 
 
 ### In case of HTML we use that function, which removes all HTML, CSS tags and JavaScript scripts
 ### Then saves it into the overall "texts.html"
-def htmlFileParse(archive, f, archivePath):
+def htmlFileParse(archive, f):
+
+	targetPath = archive.filename[:-4]
 
 	htmlContent = archive.read(f)
 	
@@ -55,43 +61,46 @@ def htmlFileParse(archive, f, archivePath):
 	
 	textContent = soup.get_text()
 
-	with open(archivePath + '/texts.html', 'a+') as target:
+	with open(targetPath + '/texts.html', 'a+') as target:
 		target.write(textContent)
 
 
 ### Extracts file from ZIP and save it in the folder it relies to (classifies it)
-def copyAndClassify(f, archivePath, archive):
+def copyAndClassify(f, archive):
+
+	targetPath = archive.filename[:-4]
+
+	### fName representes item's name with .extension (eg. mycoolimage.jpg)
 	fName = os.path.basename(f)
-	fType = fName.split('.')[-1]
 
 	### skips directories and images
-	if not fName or fType.lower() in ['jpeg', 'jpg', 'png', 'gif']:
+	### fName.split('.')[-1] represents file's extension
+
+	if (not fName) or (fName.split('.')[-1].lower() in ['jpeg', 'jpg', 'png', 'gif']):
 		return
 
-	if fType.lower() in ['html', 'htm']:
-		htmlFileParse(archive, f, archivePath)
+	if fName.split('.')[-1].lower() in ['html', 'htm']:
+		htmlFileParse(archive, f)
 		return
 
-	copyFile(archive, f, archivePath, fName)
+	copyFile(archive, f, fName)
 
-	### We request shell for finding the type of the content
-	typeRequest = subprocess.check_output('file "%s/%s"' % (archivePath, fName), shell=True).decode("utf-8") 
+	### We request shell for finding the type of the file's content
+	typeRequest = subprocess.check_output('file "%s/%s"' % (targetPath, fName), shell=True).decode("utf-8") 
 	typeName = typeRequest.split(": ")[1].replace('/', ' ')
 
-	classifyFile(typeName, archivePath, fName)
+	classifyFile(typeName, targetPath, fName)
 
 
 if __name__ == "__main__":
 
-	archivePath = "websoubory.zip"[:-4]
+	### Creates ZipFile object from our argv input
+	#archive = zipfile.ZipFile(sys.argv[1][:-4]+".zip", 'r')
+	archive = zipfile.ZipFile("websoubory.zip", 'r')
 
-	### Path to our ZIP without .zip ending
-	# archivePath = sys.argv[1][:-4]
+	### By archive.filename we can find it's relative path, for ex. 'websoubory.zip'
+	createTargetPath(archive.filename[:-4])
 
-	archive = zipfile.ZipFile(archivePath+".zip", 'r')
-
-	createTargetPath(archivePath)
-
+	### Goes through every item in ZIP
 	for f in archive.namelist():
-
-		copyAndClassify(f, archivePath, archive)
+		copyAndClassify(f, archive)
